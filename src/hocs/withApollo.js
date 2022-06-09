11239f44a9f909c612ee055b228268a756990a24
shanialy <xhanialee1@gmail.com>
2022-06-09 10:50:22 +0500

    first

--- src/hocs/withApollo.js
+++ src/hocs/withApollo.js
@@ -5,7 +5,7 @@ import { InMemoryCache, ApolloProvider, ApolloClient } from "@apollo/client";
 export default withApollo(
   ({ initialState }) => {
     return new ApolloClient({
-      uri: "http://localhost:3000/api/graphql",
+      uri: "http://localhost:3087/api/graphql",
       cache: new InMemoryCache().restore(initialState || {}),
     });
   },
