415b8d741e6c1e5a6c282aa46d3cfbab2fbacd96
shanialy <xhanialee1@gmail.com>
2022-06-09 12:42:40 +0500

    first

--- src/pages/home/twenty.js
+++ src/pages/home/twenty.js
@@ -20,7 +20,7 @@ const HomeTwenty = () => {
   return (
     <Fragment>
       <Head>
-        <title>Shani</title>
+        <title>Shanialy</title>
       </Head>
 
       <Header logo={logo} navData={navContent} containerFluid={false} />
